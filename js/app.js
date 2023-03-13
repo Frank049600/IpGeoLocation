c1cf060170a3dcd1a0d01579dbcea693943af0ad
Francisco Hernandez <paco049600@gmail.com>
2023-03-12 20:05:23 -0600

    Se muestra por boton

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 let cardsGeo = document.getElementById("cards")
 let locationGeo = document.getElementById("location").content
-let fragment = document.createDocumentFragment() 
+let fragment = document.createDocumentFragment()
+let showInput = document.querySelector("location")
+let showOutput = document.querySelector("location")
+
 
 let topGeo = []
 
@@ -21,26 +24,39 @@ const loadGeo = () => {
         .then(response => response.json())
         .then(response => {
             topGeo = response
-            console.log(topGeo)
-            mostrarGeo()
+            //console.log(topGeo)
+
+            //Muestra la información del Templade despues de dar click en el boton Buscar
+            showInput.addEventListener('onClick', () => {
+                showInput.mostrarGeo();
+            })
+
+            showOutput.addEventListener('onClick', () => {
+                showOutput.cerrarGeo();
+            })
+
         })
         .catch(err => console.error(err));
 }
 
 const mostrarGeo = () =>{
     
-    locationGeo.querySelectorAll('p')[0].textContent = topGeo.location.latitude
-    locationGeo.querySelectorAll('p')[1].textContent = topGeo.location.longitude
+    locationGeo.querySelectorAll('p')[0].textContent = "Latitud: "+topGeo.location.latitude
+    locationGeo.querySelectorAll('p')[1].textContent = "Longitud: "+topGeo.location.longitude
     locationGeo.querySelectorAll('p')[2].textContent = topGeo.city.name
     locationGeo.querySelectorAll('p')[3].textContent = topGeo.country.country_name
-    locationGeo.querySelectorAll('p')[4].textContent = topGeo.country.languages[0].native
+    locationGeo.querySelectorAll('p')[4].textContent = "Lenguaje: "+topGeo.country.languages[0].native
     locationGeo.querySelectorAll('p')[5].textContent = topGeo.continent.continent_name
-    locationGeo.querySelectorAll('p')[6].textContent = topGeo.currency.name
-    locationGeo.querySelectorAll('p')[7].textContent = topGeo.currency.code
+    locationGeo.querySelectorAll('p')[6].textContent = "Moneda: "+topGeo.currency.name
+    locationGeo.querySelectorAll('p')[7].textContent = "Código: "+topGeo.currency.code
     
 
     const clone = locationGeo.cloneNode(true)
     fragment.appendChild(clone)
 
     cardsGeo.appendChild(fragment)
+}
+
+const cerrarGeo = () => {
+    topGeo.close(cardsGeo);
 }
